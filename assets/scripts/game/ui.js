680437787044407a5863743b78a69ee8ce9e1597
jennygodley <jennifergodley@gmail.com>
2019-08-14 19:34:05 -0400

    Add a file
    Missed a file in my last commit.

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,15 +2,29 @@
 
 const store = require('../store')
 const game = require('./game')
+const events = require('./events')
 
 const boardClear = function () {
   $('.box').text('')
   $('#messages').replaceWith('<div id="messages">player X\'s turn!</div>')
-  $('#reset').text('')
-  game.player = 'X'
-  console.log(game.player)
+  $('#reset').html('')
+  game.resetBoard()
+  console.log(events.player)
+}
+
+const makeMoveOnBoard = function () {
+  $(event.target).text(store.value)
+}
+
+const displayMessage = function () {
+  if (game.checkForWin() === true) {
+    $('#messages').replaceWith('<div id="messages">player ' + store.value + ' wins!</div>')
+    $('#reset').html('<button>reset</button>')
+  }
 }
 
 module.exports = {
-  boardClear
+  boardClear,
+  displayMessage,
+  makeMoveOnBoard
 }
